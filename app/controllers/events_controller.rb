6b8b9a339875401921d0ffa8ee3c6ded27b97c42
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2025-12-15 22:04:41 +0530

    feat(views,controllers): create new events , added new event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,23 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
   def create
+    event = current_user.events.build(event_params)
+    if event.save!
+      redirect_to events_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.expect(event: [ :name, :location, :date ])
   end
 end
