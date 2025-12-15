a2837531f62d9d4bbb576d813a435cd7e607989e
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2025-12-15 21:17:07 +0530

    feat(routes): update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root "events#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,4 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  devise_for :users
+  root "events#index"
+  resources :events, only: [ :index, :new, :create ]
 end
