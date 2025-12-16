bf98e995550389643599c275945bda018b06d575
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2025-12-16 16:05:06 +0530

    feat(controller): don't authenticate for viewing index page of events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [ :show, :new, :create ]
 
   def index
     @events = Event.all
