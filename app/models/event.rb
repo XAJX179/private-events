eb001066d2751ecfa3da0e9ef2aa153b3be5bd09
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2025-12-17 14:40:14 +0530

    feat(app): Separate the past and upcoming events on the Event’s Index page

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,12 @@ class Event < ApplicationRecord
   has_many :event_attendees, dependent: :destroy,
             foreign_key: "attending_event_id"
   has_many :attendees, through: :event_attendees, class_name: "User"
+
+  def self.past
+    Event.where("date < ?", Date.today)
+  end
+
+  def self.upcoming
+    Event.where("date >= ?", Date.today)
+  end
 end
